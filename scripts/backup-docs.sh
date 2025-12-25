33fd6b0d8687853a40d35ff33d8ebec8b3d8f502
Vlad <sky-mart@hotmail.com>
2025-12-25 10:31:50 +0100

    Minor fix of the docs backup

--- scripts/backup-docs.sh
+++ scripts/backup-docs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-mkdir /tmp/docs && \
+mkdir -p /tmp/docs && \
     sshfs ftpvlad@h3007161.stratoserver.net:/home/ftpvlad /tmp/docs && \
     restic backup -r ~/backups/docs -e "\.*" /tmp/docs && \
     fusermount -u /tmp/docs
